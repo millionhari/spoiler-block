d00311d9177569690e5f5a4173635f34ce8b36fe
Mike Hari <michael_hari@apple.com>
2016-01-04 12:17:25 -0800

    (feat) Added add to storage feature
    -TODO: Convert object to array

--- src/options/scripts/options.js
+++ src/options/scripts/options.js
@@ -1,11 +1,53 @@
 (function(){
   var addButton = document.querySelector('.button-add');
+  var _wordsObjectCreator = function(arr){
+    var obj = {};
+    for (var i = 0; i < arr.length; i++){
+      obj[arr[i]] = arr[i];
+    }
+    return obj;
+  };
+
+  var _flattenObj = function(obj){
+    var finalObj = {};
+    var recurse = function(element){
+      for (var i in element){
+        if (typeof element[i] === 'object' && element !== null){
+          recurse(element[i]);
+        } else {
+          finalObj[i] = element[i];
+        }
+      }
+      return;
+    };
+    recurse(obj);
+    return finalObj;
+  };
+
+  var _combineObjects = function(currentObj, newObj){
+    for (var i in newObj){
+      currentObj[i] = newObj[i];
+    }
+    currentObj = _flattenObj(currentObj);
+    return currentObj;
+  };
+
+  // TODO: convert object to array
+  var _objToArray = function(){
+
+  };
+
+
   addButton.onclick = function(){
-    var wordsList = document.querySelector('.block-words').value.split(' ');
-    chrome.storage.sync.set({
-      wordsList: wordsList
-    }, function(){
-      console.log('success!');
+    var inputBox = document.querySelector('.block-words').value.split(' ');
+    var wordsBank = _wordsObjectCreator(inputBox);
+
+    chrome.storage.sync.get('wordsList', function(currentStorage){
+      var updatedStorage = _combineObjects(currentStorage, wordsBank);
+
+      chrome.storage.sync.set({'wordsList': updatedStorage}, function(){
+        console.log('The storage has been updated!', updatedStorage);
+      });
     });
   };
 })();
